43854a9db59702a09133ed9718e8d9cf6ad67569
Vaibhav Mangar <vaibhavmangar@Vaibhavs-MacBook-Air.local>
2024-03-09 16:25:28 +0530

    made nesserasry changes for jtask 3

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,31 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number ,
+  price_def: number ,
+  ratio: number ,
   timestamp: Date,
+  upper_bound: number ,
+  lower_bound: number ,
+  trigger_alert:number | undefined ,
 }
 
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+    const priceABC =(serverResponds[0].top_ask.price + serverResponds[0].top_bid.price)/2;
+    const priceDEF =(serverResponds[1].top_ask.price + serverResponds[1].top_bid.price)/2;
+    const ratio=priceABC/priceDEF;
+    const upper_Bound=1+0.05;
+    const lower_Bound=1-0.05;
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        price_abc: priceABC,
+        price_def: priceDEF,
+        ratio,
+        timestamp:serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp ,
+        upper_bound: upper_Bound,
+        lower_bound:lower_Bound,
+        trigger_alert:(ratio>upper_Bound|| ratio< lower_Bound)? ratio:undefined,
       };
-    })
   }
 }
